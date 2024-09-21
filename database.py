f89cfcd2e07d9a0bd615b953424abab940f62a63
Onur Solmaz <2453968+osolmaz@users.noreply.github.com>
2024-09-21 23:42:45 +0200

    Migrate from sqlite to postgres and containerize (#1)
    * Migrate from sqlite to postgres
    
    * Containerization works with docker compose

--- database.py
+++ database.py
@@ -2,8 +2,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config import DATABASE_URL
+from config import DATABASE_URL, CONNECT_ARGS
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args=CONNECT_ARGS)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
